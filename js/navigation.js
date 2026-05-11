4ef023653c21c29c466b9bf36b61aac1f00639ca
Filipa Carvalho <pipa85@gmail.com>
2026-05-12 00:48:21 +0100

    Fix navigation links for project subfolders

--- js/navigation.js
+++ js/navigation.js
@@ -1,25 +1,29 @@
 // Shared Navigation System
 (function() {
+    // Detect if we're in a subfolder
+    const inSubfolder = window.location.pathname.includes('/projects/');
+    const prefix = inSubfolder ? '../' : '';
+    
     const nav = `
         <nav class="navbar" id="navbar">
             <div class="container">
                 <div class="nav-wrapper">
-                    <a href="index.html" class="logo">Filipa Carvalho</a>
+                    <a href="${prefix}index.html" class="logo">Filipa Carvalho</a>
                     <button class="mobile-menu-toggle">
                         <span></span>
                         <span></span>
                         <span></span>
                     </button>
                     <ul class="nav-menu">
-                        <li><a href="index.html" class="nav-link">Home</a></li>
-                        <li><a href="biography.html" class="nav-link">Biography</a></li>
-                        <li><a href="research.html" class="nav-link">Research</a></li>
-                        <li><a href="publications.html" class="nav-link">Publications</a></li>
-                        <li><a href="projects.html" class="nav-link">Projects</a></li>
-                        <li><a href="people.html" class="nav-link">People</a></li>
-                        <li><a href="photos.html" class="nav-link">Photos</a></li>
-                        <li><a href="news.html" class="nav-link">News</a></li>
-                        <li><a href="contact.html" class="nav-link">Contact</a></li>
+                        <li><a href="${prefix}index.html" class="nav-link">Home</a></li>
+                        <li><a href="${prefix}biography.html" class="nav-link">Biography</a></li>
+                        <li><a href="${prefix}research.html" class="nav-link">Research</a></li>
+                        <li><a href="${prefix}publications.html" class="nav-link">Publications</a></li>
+                        <li><a href="${prefix}projects.html" class="nav-link">Projects</a></li>
+                        <li><a href="${prefix}people.html" class="nav-link">People</a></li>
+                        <li><a href="${prefix}photos.html" class="nav-link">Photos</a></li>
+                        <li><a href="${prefix}news.html" class="nav-link">News</a></li>
+                        <li><a href="${prefix}contact.html" class="nav-link">Contact</a></li>
                         <li><button id="themeToggle" class="theme-toggle" aria-label="Toggle dark mode">🌙</button></li>
                     </ul>
                 </div>
@@ -35,7 +39,8 @@
         // Set active page
         const currentPage = window.location.pathname.split('/').pop() || 'index.html';
         document.querySelectorAll('.nav-link').forEach(link => {
-            if (link.getAttribute('href') === currentPage) {
+            const href = link.getAttribute('href');
+            if (href.endsWith(currentPage)) {
                 link.classList.add('active');
             }
         });
